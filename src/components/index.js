ac3bc1a7eac7d995d974f1de27e5b3977cf05805
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-01-30 13:03:18 -0600

    re-upload files

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from './navbar';
+export { default as Navbar } from './Navbar';
 export { default as Feed } from './Feed';
 export { default as ChannelDetails } from './ChannelDetails';
 export { default as VideoDetails } from './VideoDetails';
