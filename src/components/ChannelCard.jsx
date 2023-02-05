c98969c0e19e3b41a263d56780a7b68ca5a9938f
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2023-02-05 18:39:25 +0200

    finish channelCard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,62 @@
 import React from "react";
-
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { demoProfilePicture } from "../utils/constants";
+import { Link } from "react-router-dom";
+import { CheckCircle } from "@mui/icons-material";
+const ChannelCard = ({ channelDetail }) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { sx: "356px", md: "320px" },
+        height: '326px',
+        margin: 'auto'
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}
+            </Typography>
+          )}{" "}
+          Subscribers
+        </CardContent>
+      </Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
